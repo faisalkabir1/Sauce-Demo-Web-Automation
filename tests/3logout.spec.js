520135e4e2ac48adab979a26f40ce5966cf45fa5
Faisal Kabir <onlyfaisalkabir@gmail.com>
2025-11-16 12:14:36 +0600

    logout test code add

---
+++ tests/3logout.spec.js
@@ -0,0 +1,15 @@
+const { test, expect } = require('@playwright/test');
+const { LoginPage } = require('../pages/LoginPage');
+const { MenuPage } = require('../pages/MenuPage');
+
+test('User logs out successfully', async ({ page }) => {
+  const login = new LoginPage(page);
+  const menu = new MenuPage(page);
+
+  await login.goto();
+  await login.login('standard_user', 'secret_sauce');
+await page.waitForTimeout(2000);
+  await menu.logout();
+  await expect(page).toHaveURL('/');
+  await page.waitForTimeout(2000);
+});
